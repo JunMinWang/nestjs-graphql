6641a3b438bc40d13a993586c9500f7f999e6355
Jimmy Wang <iamjunminwang@gmail.com>
2022-06-13 20:30:51 +0800

    Add query and mutation

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 ```shell
 npm i @nestjs/graphql @nestjs/apollo graphql apollo-server-express
 ```
+
+# GraphQL
+
+### Query
+
+### Mutation
+
+n REST, any request might end up causing some side-effects on the server, but by convention it's suggested that one doesn't use GET requests to modify data. GraphQL is similar - technically any query could be implemented to cause a data write. However, it's useful to establish a convention that any operations that cause writes should be sent explicitly via a mutation.
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,33 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { PetsModule } from './pets/pets.module';
+import { join } from 'path/posix';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 @Module({
-  imports: [],
+  imports: [
+    // Use code first approach
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      port: +process.env.POSTGRES_PORT,
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DATABASE,
+      synchronize: true,
+      "entities": ["dist/**/*.entity.js"]
+    }),
+    PetsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
